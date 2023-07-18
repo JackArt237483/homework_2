78429fc9200d601dce5a2f5ab2f43b590b80026f
JackArt237483 <vip.artem4567@mail.ru>
2023-07-18 06:32:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colorOne: "red",
+      colorTwo: "blue"
+    };
+  }
+
+  clickToBtn = (colorNumber) => {
+    if (colorNumber === 1) {
+      this.setState(prevState => ({
+        colorOne: prevState.colorOne === "red" ? "blue" : "red"
+      }));
+    } else if (colorNumber === 2) {
+      this.setState(prevState => ({
+        colorTwo: prevState.colorTwo === "blue" ? "red" : "blue"
+      }));
+    }
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <button onClick={() => this.clickToBtn(1)} className={this.state.colorOne}></button>
+        <button onClick={() => this.clickToBtn(2)} className={this.state.colorTwo}></button>
+      </div>
+    );
+  }
 }
-
-export default App;
